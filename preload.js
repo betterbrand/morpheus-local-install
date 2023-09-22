1a8e3b88ec96b3b0a209a08ae4fb0ec3942dbfbc
Scott Berenzweig <scott@betterbrand.com>
2023-09-21 17:08:41 -0700

    init test

--- preload.js
+++ preload.js
@@ -5,6 +5,10 @@
  *
  * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
+
+
+import { BubbleChat } from 'flowise-embed-react' 
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -15,3 +19,6 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${type}-version`, process.versions[type])
   }
 })
+
+
+const Chatbot = () => {     return (         <BubbleChat chatflowid="ece63570-5832-46e4-80ff-60ace9302df7" apiHost="https://agent-uno.onrender.com" />     ); };   return (    <Chatbot /> ) 
