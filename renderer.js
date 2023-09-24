ef9bfc7bab02a4b6e76461f8dbf1223f4fcccada
wolf <billodida420@gmail.com>
2023-09-24 23:48:02 +0300

    adding ollama process to electron

--- renderer.js
+++ renderer.js
@@ -5,3 +5,12 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+
+const func = async () => {
+    const response = await window.versions.ping()
+    console.log(response) // prints out 'pong'
+  }
+  
+func()
